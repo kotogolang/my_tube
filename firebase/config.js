133098c16bd10db35741a8ce99fcd9adfede4e8a
kotogolang <kotogolang@gmail.com>
2019-12-22 04:53:14 +0900

    Finish ch01

---
+++ firebase/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBloZgbGLmEa_YeghCRQ8QaI0-lHODbalQ",
+    authDomain: "nekotube-5ff2a.firebaseapp.com",
+    databaseURL: "https://nekotube-5ff2a.firebaseio.com",
+    projectId: "nekotube-5ff2a",
+    storageBucket: "nekotube-5ff2a.appspot.com",
+    measurementId: "G-NS8HF183JK"
+};
